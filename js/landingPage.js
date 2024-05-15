5e201eab9cbb080d7962ddb85dc49454f0734d9c
yammies24 <58029701+yammies24@users.noreply.github.com>
2024-05-15 21:30:59 +0800

    Add files via upload

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,27 +1,75 @@
 var pcMedia = window.matchMedia("(min-width: 701px)");
 var activeMobile = document.querySelector(".mobile");
 var burger = document.querySelector("#burger");
-var burgerInput = document.querySelector("input");
-var mobileMenu = document.querySelectorAll(".mobile a li");
-//FUNCTION FOR MEDUA QUERY CHANGES EVENT
+var burgerInput = document.querySelectorAll("input");
+var mobileMenu = document.querySelectorAll(".mobile-list");
+var toggleMode = document.querySelector(".container");
+var toggleModes = document.querySelector(".input");
+var body = document.querySelector("body");
+var list = document.querySelectorAll("li");
+var nav = document.querySelector("nav");
+var aTag = document.querySelectorAll("a");
+var spanTag = document.querySelectorAll("span");
+
+//FUNCTION FOR MEDUA QUERY CHANGES EVENT BREAKPOINT
 function medias(pcMedia){
-
         activeMobile.classList.remove("active");
-        burgerInput.checked = false;
-        
-}
+        burgerInput[1].checked = false;
+        toggleMode.classList.remove("mobile-container-toggle-button");
 
+}
 pcMedia.addEventListener("change",
 () => {
-    
     medias(pcMedia);
-
 });
 
+
+//CLICK EVENT
+function click(burger){
+    activeMobile.classList.toggle("active");
+    if(document.querySelector(".light")){
+        activeMobile.style.backgroundColor = "var(--whtie)";
+        activeMobile.style.borderLeft = "1px solid var(--darkgreen)";
+    }
+    
+};
 //FUNCTION FOR BURGER CLICK EVENT
 burger.addEventListener("click", () => {
+    click(burger);
+});
+function bodyLightMode(toggleModes){
+    body.classList.toggle("light");
+}
+function liLightMode(toggleModes){
+    for(var listHeader = 0; listHeader <= list.length; listHeader++){
+        list[listHeader].classList.toggle("li-lightMode");
+        aTag[listHeader].classList.toggle("nav-a-li-lightMode");
+    };
+    
+};
 
-    activeMobile.classList.toggle("active");
+toggleModes.addEventListener("click",()=>{
+    bodyLightMode(toggleModes);
+    if(document.querySelector(".light")){
+        nav.classList.toggle("nav-lightMode");
+        liLightMode(toggleModes);
+        
+    }
+    else{
+        nav.classList.remove("nav-lightMode");
+        activeMobile.style.backgroundColor = "var(--darkgreen)";
+        for(var listHeader = 0; listHeader <= list.length; listHeader++){
+            list[listHeader].classList.remove("li-lightMode");
+            aTag[listHeader].classList.remove("nav-a-li-lightMode");
+        };
+
+        // LIST INORDER BUTTON OFF
+        for(var i = 0; i <= list.length; i++){
+            document.querySelectorAll("li")[i].style.animationName = "listOff";
+            document.querySelectorAll("li")[i].style.color = "var(--whtie)";
+            
+        };
+    }
 
 });
 
@@ -29,9 +77,9 @@ burger.addEventListener("click", () => {
 window.addEventListener("scroll", () =>{
 
     document.querySelector("nav").classList.toggle("scroll",window.scrollY > 300);
+    toggleMode.classList.toggle("mobile-container-toggle-button",window.scrollY > 300)
     document.querySelector(".logo").classList.toggle("logoScroll",window.scrollY > 300);
     document.querySelector(".pop").classList.toggle("active",window.scrollY > 600);
-
 
 });
 
@@ -41,8 +89,8 @@ for(var menuCount =0; menuCount <= mobileMenu.length; menuCount++){
     mobileMenu[menuCount].addEventListener("click", ()=>{
 
         activeMobile.classList.toggle("active");
-
-        burgerInput.checked = false;
+        burgerInput[1].checked = false;
+        
 
 });
 };
